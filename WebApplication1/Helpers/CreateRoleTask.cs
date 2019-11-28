1431e8b2ffec08387a44512057c6f182ad13e728
Anton-Volchkov <anton.volchkov.2001@gmail.com>
2019-11-28 15:49:13 +0300

    Выдать себе админку

--- WebApplication1/Helpers/CreateRoleTask.cs
+++ WebApplication1/Helpers/CreateRoleTask.cs
@@ -1,16 +1,18 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using SlvTeam.Domain.Entities;
 
 namespace WebApplication1.Helpers
 {
     public class CreateRolesTask
     {
         private readonly RoleManager<IdentityRole> _manager;
+        private readonly UserManager<SlvTeamUser> _userManager;
 
-
-        public CreateRolesTask(RoleManager<IdentityRole> manager)
+        public CreateRolesTask(RoleManager<IdentityRole> manager, UserManager<SlvTeamUser> userManager)
         {
             _manager = manager;
+            _userManager = userManager;
         }
 
         public async Task Execute()
@@ -24,6 +26,10 @@ namespace WebApplication1.Helpers
 
             var role = new IdentityRole(roleName);
             await _manager.CreateAsync(role);
+
+            var user = await _userManager.FindByNameAsync("Kerli");
+            await _userManager.AddToRoleAsync(user, "Admin");
+
         }
     }
 }
