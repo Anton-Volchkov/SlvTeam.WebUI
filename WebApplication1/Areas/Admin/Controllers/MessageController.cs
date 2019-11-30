6df716f1585d81e9e99ac6452de822c9c2c9aeee
Anton-Volchkov <anton.volchkov.2001@gmail.com>
2019-12-01 00:30:02 +0300

    Добавить функцию очистки сообщений для администратора

---
+++ WebApplication1/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SlvTeam.Application.Chat.Commands.DeleteAllMessage;
+
+namespace SlvTeam.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class MessageController : Controller
+    {
+        private readonly IMediator _mediator;
+        public MessageController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteAllMessage()
+        {
+            await _mediator.Send(new DeleteAllMessageCommand());
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
